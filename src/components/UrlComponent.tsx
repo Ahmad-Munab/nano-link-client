2cf3a193f0e4dc73d5e77bfebe18e4fcea64e60a
Ahmad Munab <ahmadmunab22@gmail.com>
2023-05-06 19:44:22 +0600

    added custom interface

--- src/components/UrlComponent.tsx
+++ src/components/UrlComponent.tsx
@@ -1,3 +1,5 @@
+
+
 const API_BASE = import.meta.env.VITE_API_BASE;
 import { BsClipboardCheck, BsClipboard } from "react-icons/bs";
 import { motion } from "framer-motion";
@@ -27,6 +29,7 @@ const UrlComponent = ({ url }: any) => {
     setCopied(true);
     setTimeout(() => setCopied(false), 3000);
   }
+  const nanoLink = `${API_BASE}/${url.short}`;
 
   return (
     <div className="flex xl:flex-row flex-col gap-4 justify-between items-center bg-white py-4 2xl:px-8 md:px-4 px-3 rounded-md shadow-lg  mx-32 my-8 text-2xl font-semibold">
@@ -34,15 +37,14 @@ const UrlComponent = ({ url }: any) => {
       <div className="flex items-center gap-2">
         <span
           onClick={() => {
-            window.open(`${API_BASE}/${url.short}`, "_blank");
+            window.open(nanoLink, "_blank");
           }}
           className="  hover:underline font-sans text-blue-600 cursor-pointer"
         >
-          {API_BASE}/{url.short}
+          {API_BASE.replace("http://", "")}/{url.short}
         </span>
         <motion.button
           onClick={copy}
-          
           whileHover={{ scale: 1.05 }}
           className="flex text-xl mx-2 transition-all duration-150 ease-in-outflex border-2 border-green-600 bg-green-200 hover:bg-green-300 py-1 px-2 rounded-lg shadow-xl"
         >
